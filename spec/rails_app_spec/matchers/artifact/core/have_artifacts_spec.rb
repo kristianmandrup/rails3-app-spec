8cdd49727afe38aacfc72d40d3f2157a570aabc2
Kristian Mandrup <kmandrup@gmail.com>
2010-09-20 21:36:29 +0200

    enabled core artifact spec

--- spec/rails_app_spec/matchers/artifact/core/have_artifacts_spec.rb
+++ spec/rails_app_spec/matchers/artifact/core/have_artifacts_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 root_dir = Rails3::Assist::Directory.rails_root
 
 describe 'controller' do
+  use_orm :none
   use_helpers :controller, :app
   
   before :each do              
@@ -19,25 +20,31 @@ describe 'controller' do
     remove_controllers :account, :person
   end
 
-  # it "should not have :controller artifact :person" do      
-  #   root_dir.should_not have_artifact :person, :controller
-  # end
-  #   
-  # it "should have an account_controller file that contains an AccountController class with an index method inside" do      
-  #   root_dir.should have_artifact :account, :controller do |content|
-  #     content.should have_method :index
-  #   end
-  # 
-  #   create_controller :person do
-  #     %q{
-  #       def index
-  #       end
-  #     }
-  #   end    
-  # 
-  #   root_dir.should have_artifacts :controller, :account, :person
-  #   root_dir.should have_artifacts :controller, [:account, :person]
-  # end
+  it "should not have :controller artifact :person" do      
+    root_dir.should_not have_artifact :person, :controller
+  end
+
+  it "should have model" do
+    create_model :user
+    root_dir.should have_artifact :user, :model
+    root_dir.should have_model :user
+  end
+    
+  it "should have an account_controller file that contains an AccountController class with an index method inside" do      
+    root_dir.should have_artifact :account, :controller do |content|
+      content.should have_method :index
+    end
+  
+    create_controller :person do
+      %q{
+        def index
+        end
+      }
+    end    
+  
+    root_dir.should have_artifacts :controller, :account, :person
+    root_dir.should have_artifacts :controller, [:account, :person]
+  end
   
   it "should not have :controller artifacts :account and :user" do       
     root_dir.should_not have_artifacts :controller, [:account, :user]    
